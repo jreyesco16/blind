55f12709412eccb87fc578e8c2b6384eb1a036cb
jreyesco16 <jesserc.2@gmail.com>
2022-08-15 19:57:19 -0500

    working solution for meadian from data stream

---
+++ Heap_PriorityQueue/median_from_data_stream.py
@@ -0,0 +1,24 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.l = []
+
+    def addNum(self, num: int) -> None:
+        self.l.append(num)
+
+    def findMedian(self) -> float:
+        self.l.sort()
+
+        mid = len(self.l)//2
+
+        if mid % 2 == 0:
+            return self.l[mid]
+        else:
+            return (self.l[mid]+self.l[mid+1])/2
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
