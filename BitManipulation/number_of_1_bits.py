16e337153ff8b557a36729bc2cbe34cb06a71704
jreyesco16 <jesserc.2@gmail.com>
2022-09-10 08:05:08 -0500

    optimal solution for valid anagram

--- BitManipulation/number_of_1_bits.py
+++ BitManipulation/number_of_1_bits.py
@@ -1,8 +1,11 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         
-        bit_str = '{0:08b}'.format(n)
-        
-        bit_str = bit_str.replace("0", "")
-        
-        return len(bit_str)
+        res = 0
+
+        while n:
+            res += n % 2
+
+            n = n >> 1
+
+        return res
