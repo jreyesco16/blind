e070ba1b413e7abe95d4e6c4b6a38350b2bb08d4
jreyesco16 <jesserc.2@gmail.com>
2022-09-12 20:05:29 -0500

    git is dumb

--- Tries/deign_add_and_search_words_data_structure.py
+++ Tries/deign_add_and_search_words_data_structure.py
@@ -1,12 +1,37 @@
 class WordDictionary:
+
     def __init__(self):
-        
+        self.l = []
 
     def addWord(self, word: str) -> None:
-        
+        self.l.append(word)
 
     def search(self, word: str) -> bool:
         
+        # query 
+        if '.' in word:
+            match = False
+            
+            for w in self.l:
+                if len(w) != len(word):
+                    continue
+                else:
+                    match = True
+                    
+                    for i in range(len(word)):
+                        if word[i]=='.':
+                            continue
+                        elif word[i]!=w[i]:
+                            match = False
+                    if match:
+                        return match
+                
+            return match
+        
+        elif word in self.l:
+            return True
+        else:
+            return False
 
 
 # Your WordDictionary object will be instantiated and called as such:
