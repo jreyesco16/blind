b258c561ab5e92609044b8f0dcc235f30eb9d532
jreyesco16 <jesserc.2@gmail.com>
2022-09-24 13:16:06 -0500

    refactored contains duplicates solution

--- ArraysAndHashing /contains_duplicates.py
+++ ArraysAndHashing /contains_duplicates.py
@@ -1,25 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
-        
-        s = set(nums)
-
-        if len(nums) == len(s):
-            return False
-        
-        return True
-
-def main():
-    test_case = [1,2,3,1]
-
-    expected = True
-
-    result = Solution().containsDuplicate(test_case)
-
-    if result == expected:
-        print("PASSED")
-    else:
-        print("FAILED")
-
-
-if __name__ == "__main__":
-    main()
+        return not len(nums) == len(set(nums))
