2c413aa632ff79070da3bd1d3b3284b2181ae4d6
jreyesco16 <jesserc.2@gmail.com>
2022-09-24 14:18:14 -0500

    refactored valid palindrome

--- TwoPointers/valid_palindrome.py
+++ TwoPointers/valid_palindrome.py
@@ -1,38 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
-
         lowers = s.lower()
 
-        fancy_str = ''
+        alphanum_str = ''
 
-        for x in lowers:
-            if x in alphabet:
-                fancy_str += x
+        for c in lowers:
+            if c.isalnum():
+                alphanum_str += c
         
-        pivot = int(len(fancy_str)/2)
-
-        for i in range(0, pivot):
-            if fancy_str[i] != fancy_str[len(fancy_str)-1-i]:
-                return False
-        
-        return True
-
-
-def main():
-
-    test_case = "0P"
-
-    expected = False
-
-    result = Solution().isPalindrome(test_case)
-
-    if expected == result:
-        print("PASSED")
-    else:
-        print("FAILED")
-
-
-if __name__ == "__main__":
-    main()
+        return alphanum_str == alphanum_str[::-1]
