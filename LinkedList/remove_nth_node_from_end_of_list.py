0ad169b99d25e5d0bd6414e51300d325b065918f
jreyesco16 <jesserc.2@gmail.com>
2022-10-13 21:35:43 -0500

    optimal solution for remove nth node from end of list

--- LinkedList/remove_nth_node_from_end_of_list.py
+++ LinkedList/remove_nth_node_from_end_of_list.py
@@ -5,29 +5,18 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # traverse linked list
-        s = []
-        while head:            
-            s.append(head)
-            head = head.next
-            
-        # remove index
-        index = len(s)-n
-        
-        # edge case remove first link
-        if index == 0:
-            if 1 < len(s):
-                return s[1]
-            else:
-                return None
-        # edge case remove last link
-        if index == len(s)-1:
-            if 1 < len(s):
-                s[-2].next = None
-                return s[0]
-            else: 
-                return None
-            
-        s[index-1].next = s[index+1]
-        
-        return s[0]
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+
+        return dummy.next
