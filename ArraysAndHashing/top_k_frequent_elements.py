f86e63b4e5aace284d0dada6af7ba2de2f10c3bf
Jesse R Cortes <jesserc.2@gmail.com>
2023-06-17 15:39:46 -0500

    optimal solution

--- ArraysAndHashing/top_k_frequent_elements.py
+++ ArraysAndHashing/top_k_frequent_elements.py
@@ -1,10 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         d = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums: d[n] = d.get(n, 0) + 1
         
-        for i in nums:
-            d[i] = d.get(i, 0) + 1
+        for n, c in d.items(): freq[c].append(n)
 
-        sorted_d = dict(sorted(d.items(), key=operator.itemgetter(1),reverse=True))
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
 
-        return list(sorted_d.keys())[:k]
+        return res
