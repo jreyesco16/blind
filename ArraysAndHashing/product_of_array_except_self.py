30c9aa4764ce4cd4f143741eb376cf00a79ae31d
Jesse R Cortes <jesserc.2@gmail.com>
2023-06-20 19:30:13 -0500

    custom solution

--- ArraysAndHashing/product_of_array_except_self.py
+++ ArraysAndHashing/product_of_array_except_self.py
@@ -2,14 +2,31 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        l = len(nums)
-        pre, pos = 1, 1
-        pa = [1]*l
+        pre, pos = 1,1
 
-        for i in range(l):
-            pa[i] *= pre
-            pre = pre*nums[i]
-            pa[l-i-1] *= pos
-            pos *= nums[l-i-1]
+        pre_list, pos_list = [None] * len(nums), [None] * len(nums)
 
-        return pa
+        for i in range(len(nums)):
+            pre_list[i] = pre * nums[i]
+
+            pre = pre_list[i]
+
+
+            pos_list[(len(nums)-1)-i] = pos * nums[(len(nums)-1)-i]
+
+            pos = pos_list[(len(nums)-1)-i]
+
+        res_list = [1] * len(nums)
+
+        for i in range(len(res_list)):
+            if i == 0:
+                res_list[i] = pos_list[i+1]
+                continue
+            elif i == len(nums)-1:
+                res_list[i] = pre_list[i-1]
+                continue
+
+            res_list[i] = pre_list[i-1] * pos_list[i+1]
+
+
+        return res_list
