120a1e10b44a0cb26bd111c33139c3f7df027a9f
Jesse R Cortes <jesserc.2@gmail.com>
2023-06-22 19:04:42 -0500

    cleaner solution

--- ArraysAndHashing/product_of_array_except_self.py
+++ ArraysAndHashing/product_of_array_except_self.py
@@ -2,31 +2,26 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pre, pos = 1,1
+        pre, pos = 1, 1
 
-        pre_list, pos_list = [None] * len(nums), [None] * len(nums)
+        pre_nums, pos_nums = [None]*len(nums), [None]*len(nums)
 
         for i in range(len(nums)):
-            pre_list[i] = pre * nums[i]
+            pre *= nums[i]
 
-            pre = pre_list[i]
+            pre_nums[i] = pre
 
-
-            pos_list[(len(nums)-1)-i] = pos * nums[(len(nums)-1)-i]
-
-            pos = pos_list[(len(nums)-1)-i]
-
-        res_list = [1] * len(nums)
-
-        for i in range(len(res_list)):
+        for i in range(len(nums)-1, -1, -1):
+            pos *= nums[i]
+            
+            pos_nums[i] = pos
+        
+        for i in range(len(nums)):
             if i == 0:
-                res_list[i] = pos_list[i+1]
-                continue
+                nums[i] = pos_nums[i+1]
             elif i == len(nums)-1:
-                res_list[i] = pre_list[i-1]
-                continue
+                nums[i] = pre_nums[i-1]
+            else:
+                nums[i] = pre_nums[i-1] * pos_nums[i+1]
 
-            res_list[i] = pre_list[i-1] * pos_list[i+1]
-
-
-        return res_list
+        return nums
