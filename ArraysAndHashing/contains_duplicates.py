f7c6b73dfe639129119a3db119f3b4984bf4d881
Jesse R Cortes <jesserc.2@gmail.com>
2023-06-24 14:10:13 -0500

    simplified solution

--- ArraysAndHashing/contains_duplicates.py
+++ ArraysAndHashing/contains_duplicates.py
@@ -2,10 +2,10 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         s = set()
 
-        for i in nums:
-            if i in s:
+        for n in nums:
+            if n in s:
                 return True
-            else:
-                s.add(i)
+            
+            s.add(n)
 
         return False
