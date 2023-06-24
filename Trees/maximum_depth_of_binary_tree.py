f397d15578a0ccef65303086474be58dd00f50ba
Jesse R Cortes <jesserc.2@gmail.com>
2023-06-24 14:57:41 -0500

    simplified solution

--- Trees/maximum_depth_of_binary_tree.py
+++ Trees/maximum_depth_of_binary_tree.py
@@ -1,29 +1,27 @@
+from typing import Optional
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
+    def __init__(self):
+        self.d = 0
+
+    def helper(self, root: Optional[TreeNode], d: int) -> None:
+        if root == None:
+            self.d = max(self.d, d)
+            return
+
+        d += 1
+
+        self.helper(root.left, d)
+        self.helper(root.right, d)
+
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+        self.helper(root, 0)
 
-        
-        level = 0
-        
-        q = deque([root])
-
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-
-                if node.left:
-                    q.append(node.left)
-
-                if node.right:
-                    q.append(node.right)
-
-            level += 1
-
-        return level
+        return self.d
