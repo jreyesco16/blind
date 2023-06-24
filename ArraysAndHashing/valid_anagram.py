8a94da552ebcb87c128f337b848436b3af665ede
Jesse R Cortes <jesserc.2@gmail.com>
2023-06-24 14:00:25 -0500

    simplified but slower time complexity

--- ArraysAndHashing/valid_anagram.py
+++ ArraysAndHashing/valid_anagram.py
@@ -1,19 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        d = {}
-
-        for i in s: d[i] = d.get(i, 0) + 1
-
-        for i in t:
-            if i not in d:
-                return False
-
-            if d[i] == 0:
-                return False
-                
-            d[i] -= 1
-
-        return True
+        return ''.join(sorted(s)) == ''.join(sorted(t))
